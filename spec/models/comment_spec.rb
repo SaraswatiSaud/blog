1aa2fef7078975f95642ef1d3afe67429cd14be8
Saraswati Saud <saraswati.saud@gmail.com>
2017-07-14 15:13:53 +0545

    Rspec test for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe Comment, type: :model do
 
   it 'is invalid without commenter' do
     @comment.commenter = nil
-    expect(@comment).to_not be_valid
+    @comment.valid?
+    expect(@comment.errors[:commenter]).to include("can't be blank")
   end
 
   it 'is invalid without body' do
@@ -24,6 +25,11 @@ RSpec.describe Comment, type: :model do
   it 'must be atleast 5 characters long' do
     @comment.body = 'Atleat 5 characters long'
     expect(@comment).to be_valid
+  end
+
+  it 'is invalid if less than 5 characters long' do
+    @comment.body = 'four'
+    expect(@comment).to_not be_valid
   end
 
   it 'is invalid without article_id' do
